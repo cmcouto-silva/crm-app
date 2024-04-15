9a85f5d0442618ec53617f3817a98ba7f5a85ac4
Cainã Silva <cmcouto.silva@gmail.com>
2024-04-15 19:51:14 -0300

    update app

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Customer Service Predictin App
 
-Goal: Predict which customers are going to call the Customer Service, based on their past behaviour.
-
+Goal: Predict which customers are going to call the Customer Service, based on their past behaviour.  
 For a given day, predict whether a customer will call the Customer Service in the following 14 days.
--- app.py
+++ app.py
@@ -3,10 +3,53 @@ import pandas as pd
 import streamlit as st
 
 # Page config
-st.set_page_config(page_title='CRM App')
+st.set_page_config(page_title='CRM App', page_icon="https://www.betssongroup.com/wp-content/uploads/2018/09/cropped-Betsson-Group-icon-192x192.png")
+
+# Logo Betsson
+col1, col2, col3 = st.columns([1,6,1])
+with col2:
+    st.markdown("![Betsson logo](https://www.betssongroup.com/wp-content/uploads/2024/01/Betsson-Group-Official-Logo.png)")
 
 # Page title
 st.title('Customer Service Prediction')
+
+# Project description
+st.markdown("""
+##### Business Problem
+            
+For efficiency purposes, Betsson is trying to predict which customers are going to call the Customer Service, based on their past behaviour.
+
+##### Analytical problem
+            
+For a given day, predict whether a customer will call the Customer Service in the following 14 days.              
+""")
+
+st.markdown('##### Feature catalog')
+
+with st.expander('Check the feature descriptions'):
+    st.write("""
+        - **days_since_last_SE_GI:** Days since last self-exclusion/gambling issue event.
+        - **days_g2:** "Number of days with activity during week 2.
+        - **gw_g8:** Total game win amount in EUR during week 8.
+        - **gw_g10:** Total game win amount in EUR during week 10.
+        - **to_l5_l20:** Turnover in the last 5 days vs turnover in the last 20 days.
+        - **GOC_to_g9:** Total GOC turnover amount in EUR during week 9.
+        - **ini_bon_g10:** Number of bonuses during week 10.
+        - **turnover_last_20days:** Turnover amount in EUR in the last 20 days.
+        - **succ_dep_g10:** Total successful deposit amount in EUR during week 10.
+        - **succ_dep_cnt_g9:** "Number of bonuses during week 9.
+        - **SE_GI_total_70days:** Number of times the customer was either self excluded or tagged as gambling issue in the past 70 days.
+        - **SE_GI_max_datediff:** Max of distance in days between multiple self-exclusion events.
+""")
+
+st.markdown('---')
+
+st.title('What if prediction')
+
+# -- Model -- #
+
+with open('models/model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
 # -- Parameters -- #
 
@@ -30,10 +73,7 @@ with col2:
     SE_GI_max_datediff = st.number_input(label='SE_GI_max_datediff', value=-1, min_value=-1, max_value=5015)
 
 
-# -- Model -- #
-
-with open('models/model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+# -- What If Prediction -- #
 
 def prediction():
     df_input = pd.DataFrame([dict(
@@ -60,3 +100,27 @@ if st.button('Predict'):
         st.error(f'{y_pred} ({y_score:.2%})')
     else:
         st.success(f'{y_pred} ({y_score:.2%})')
+
+# -- Batch Prediction -- #
+
+st.title("Batch prediction")
+
+data = st.file_uploader('Upload your file')
+if data:
+    df_input = pd.read_csv(data)
+    predicted_probabilities = model.predict_proba(df_input)
+    df_output = (
+        df_input
+        .assign(
+            proba_0 = predicted_probabilities[:,0],
+            proba_1 = predicted_probabilities[:,1],
+            prediction = model.predict(df_input)
+            )
+        )
+
+    st.markdown('Predicted results:')
+    st.write(df_output)
+    st.download_button(
+        label='Download CSV', data=df_output.to_csv(index=False).encode('utf-8'),
+        mime='text/csv', file_name='predictions.csv'
+        )
